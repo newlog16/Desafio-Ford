0d9fbcf416ddd388db9570a5d38a226de5209e3f
Daniel Alves de Araujo <araujodaniel321@gmail.com>
2021-12-01 11:40:04 -0300

    splits the vim

--- challenge.js
+++ challenge.js
@@ -1,3 +1,28 @@
 var input = require('fs').readFileSync('stdin', 'utf8');
 
-console.log (input);
+separated = input.split('');
+
+input1 = [];
+input2 = [];
+input3 = [];
+
+for (let i = 0; i < separated.length; i++) {
+    if (i < 3) {
+        input1 = input1 + separated[i]
+    } else
+    if (i < 9) {
+        input2 = input2 + separated[i]
+    } else
+    if (i < 16) {
+        input3 = input3 + separated[i]
+    }
+    console.log(separated[i])
+
+
+}
+
+
+console.log(input1)
+console.log(input2)
+console.log(input3)
+
