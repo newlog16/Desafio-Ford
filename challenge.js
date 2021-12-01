4a3a588b52b073c29908993eab5f1747d89d73b1
Daniel Alves de Araujo <araujodaniel321@gmail.com>
2021-12-01 11:57:01 -0300

    trying to criptograph first part od vim

--- challenge.js
+++ challenge.js
@@ -10,19 +10,27 @@ for (let i = 0; i < separated.length; i++) {
     if (i < 3) {
         input1 = input1 + separated[i]
     } else
-    if (i < 9) {
-        input2 = input2 + separated[i]
-    } else
-    if (i < 16) {
-        input3 = input3 + separated[i]
-    }
+        if (i < 9) {
+            input2 = input2 + separated[i]
+        } else
+            if (i < 16) {
+                input3 = input3 + separated[i]
+            }
     console.log(separated[i])
 
 
 }
 
+var decInput = input1.split('')
+for (let j = 0; j < decInput.length; j++) {
+    
+    console.log(parseInt(decInput[j]))
+    // if (typeof(decInput[j]) = string)
 
-console.log(input1)
-console.log(input2)
+}
+
+
+console.log(parseInt(decInput[0]))
+console.log(decInput)
 console.log(input3)
 
